792ab4fc8887bd259380cac315d4048a3ccf0163
Unknown <anirudhsriram1995@gmail.com>
2019-04-15 18:54:27 +0530

    init - part of one PS. basic drawing setup

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server =http.Server(app);
+var io = socketIO(server);
+
+app.set('port',5000);
+app.use('/static',express.static(__dirname + '/static'));
+
+app.get('/',(request,response)=>{
+    response.sendFile(path.join(__dirname,'/static/index.html'));
+})
+
+
+server.listen(5000,()=>console.log("connected"));
+
+
+//sockets
+var users = {};
+io.on('connection',(socket)=>{
+   socket.on('new user',()=>{
+       users[socket.id]={
+           xPoints : [],
+           yPoints : [] ,
+           dragging:[],
+           paint:false
+       };
+   });
+   socket.on('movement',(data)=>{
+       var user  = users[socket.id]||{};
+       user.xPoints = data.xPoints ;
+       user.yPoints = data.yPoints;
+       user.dragging = data.dragging;
+       user.paint = data.paint;
+   });
+})
+setInterval(()=>{io.sockets.emit('data',users)},1000/60);
---
+++ static/game.js
@@ -0,0 +1,72 @@
+var socket = io();
+socket.emit('new user');
+
+var movement = {
+    xPoints: [],
+    yPoints: [],
+    dragging: [],
+    paint: false
+}
+function draw() {
+    $("#canvas").mousedown((e) => {
+        console.log(e);
+        movement.xPoints.push(e.clientX);
+        movement.yPoints.push(e.clientY);
+        movement.paint = true;
+        movement.dragging.push(false);
+        
+    });
+
+    $("#canvas").mouseup((e) => {
+        movement.paint = false;
+        
+    })
+
+    $("#canvas").mousemove((e) => {
+        if (movement.paint) {
+            movement.xPoints.push(e.clientX);
+            movement.yPoints.push(e.clientY);
+            movement.dragging.push(true);
+            
+        }
+    })
+
+       $("#canvas").mouseleave((e)=>{
+           movement.paint = false ;
+           
+       })
+
+       socket.on('data',(users)=>{
+        var canvas = document.getElementById('canvas');
+        context = canvas.getContext("2d");
+        for(var person in users){
+            var user = users[person] ;
+            for (var i = 0; i < user.xPoints.length; i++) {
+                context.beginPath();
+                if (user.dragging[i] && i) {
+                    context.moveTo(user.xPoints[i - 1], user.yPoints[i - 1]);
+                } else {
+                    context.moveTo(user.xPoints[i] - 1, user.yPoints[i]);
+                }
+                context.lineTo(user.xPoints[i], user.yPoints[i]);
+                context.closePath();
+                context.stroke();
+            }
+        }
+
+       })
+    function redraw() {
+        
+        context.clearRect(0, 0, 500, 500);
+        context.strokeStyle = "#df4b26";
+            context.lineJoin = "round";
+            context.lineWidth = 5;
+
+        
+        
+    }
+
+
+
+}
+setInterval(function(){socket.emit('movement',movement)},1000/60);
